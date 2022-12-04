d4760b24d08d2eb690d899a9eca1009d26cd74b3
Edwin <edwin.orjales@acadiahealthcare.com>
2022-12-03 23:53:27 -0800

    host tweaks for non-localhost

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://cpp3560.edwin-dev:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
